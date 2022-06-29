dc908ce74f681caa64744dc7ace333c5f292944c
unknown <shodiyatimilehin@gmail.com>
2022-06-29 01:53:10 +0100

    Mongoose errors handling and logging. Security middleware added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
-const authenticateUser = require('./middleware/authentication')
+
 
 // Connect DB
 const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication')
 
 // routers
 const authRouter = require('./routes/auth')
@@ -15,7 +22,18 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
